2cd96edfc30d48cd412eee53084f3d9a0c9aef20
shimshir <me.admir@gmail.com>
2016-11-03 23:01:48 +0100

    Removed console.log

--- src/main/resources/static/reactsrc/reducers/placeOrderFormReducer.js
+++ src/main/resources/static/reactsrc/reducers/placeOrderFormReducer.js
@@ -5,7 +5,6 @@ const placeOrderFormReducer = (placeOrderFormState = {pickupType: 'COLLECT'}, ac
         case UPDATE_PLACE_ORDER_FORM_ACTION:
             const newForm = {...placeOrderFormState};
             newForm[action.input.id] = action.input.value;
-            console.log(newForm);
             return newForm;
         default:
             return placeOrderFormState;
--- src/main/resources/static/reactsrc/reducers/productsReducer.js
+++ src/main/resources/static/reactsrc/reducers/productsReducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_PRODUCTS_ACTION, RECEIVE_PRODUCT_ACTION } from '../actions/actions.js';
+import {RECEIVE_PRODUCTS_ACTION, RECEIVE_PRODUCT_ACTION} from "../actions/actions.js";
 
 const productsReducer = (productsState = [], action) => {
     switch (action.type) {
@@ -7,7 +7,6 @@ const productsReducer = (productsState = [], action) => {
         case RECEIVE_PRODUCT_ACTION:
             const filteredProducts = productsState.filter(product => product.code != action.product.code);
             filteredProducts.push(action.product);
-            console.log(filteredProducts);
             return filteredProducts;
         default:
             return productsState
