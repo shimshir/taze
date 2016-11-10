22e5151fc4e0b1f794a146d3f410aa51049af6d9
shimshir <me.admir@gmail.com>
2016-11-10 23:25:58 +0100

    Added webpack plugin to define production build.

--- src/main/resources/static/webpack.config.js
+++ src/main/resources/static/webpack.config.js
@@ -22,6 +22,9 @@ module.exports = {
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
             compress: { warnings: false }
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
         })
     ]
 };
