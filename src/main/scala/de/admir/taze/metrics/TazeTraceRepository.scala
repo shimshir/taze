b5ae4d9928b5bfafff5259f3ac70a964b9cfe466
shimshir <me.admir@gmail.com>
2017-01-23 16:51:15 +0100

    Reverted json logging

--- src/main/scala/de/admir/taze/metrics/TazeTraceRepository.scala
+++ src/main/scala/de/admir/taze/metrics/TazeTraceRepository.scala
@@ -2,7 +2,7 @@ package de.admir.taze.metrics
 
 import java.util
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.gson.Gson
 import org.slf4j.LoggerFactory
 import org.springframework.boot.actuate.trace.{InMemoryTraceRepository, Trace, TraceRepository}
 import org.springframework.stereotype.Component
@@ -10,13 +10,13 @@ import org.springframework.stereotype.Component
 @Component
 case class TazeTraceRepository() extends TraceRepository {
   private val logger = LoggerFactory.getLogger(TazeTraceRepository.getClass)
-  private val objectMapper = new ObjectMapper()
+  private val gson = new Gson()
   private val inMemoryTraceRepository = new InMemoryTraceRepository()
 
   override def findAll(): util.List[Trace] = inMemoryTraceRepository.findAll()
 
   override def add(traceInfo: util.Map[String, AnyRef]): Unit = {
-    logger.info(objectMapper.writeValueAsString(traceInfo))
+    logger.info(gson.toJson(traceInfo))
     inMemoryTraceRepository.add(traceInfo)
   }
 }
