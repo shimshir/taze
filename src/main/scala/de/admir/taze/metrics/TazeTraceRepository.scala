a8f0136a7cfd6b2365904a4d4f0c5bd540fabce3
shimshir <me.admir@gmail.com>
2017-01-23 16:44:44 +0100

    Loggly json

--- src/main/scala/de/admir/taze/metrics/TazeTraceRepository.scala
+++ src/main/scala/de/admir/taze/metrics/TazeTraceRepository.scala
@@ -2,7 +2,7 @@ package de.admir.taze.metrics
 
 import java.util
 
-import com.google.gson.Gson
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import org.springframework.boot.actuate.trace.{InMemoryTraceRepository, Trace, TraceRepository}
 import org.springframework.stereotype.Component
@@ -10,13 +10,13 @@ import org.springframework.stereotype.Component
 @Component
 case class TazeTraceRepository() extends TraceRepository {
   private val logger = LoggerFactory.getLogger(TazeTraceRepository.getClass)
-  val gson = new Gson()
-  val inMemoryTraceRepository = new InMemoryTraceRepository()
+  private val objectMapper = new ObjectMapper()
+  private val inMemoryTraceRepository = new InMemoryTraceRepository()
 
   override def findAll(): util.List[Trace] = inMemoryTraceRepository.findAll()
 
   override def add(traceInfo: util.Map[String, AnyRef]): Unit = {
-    logger.info(gson.toJson(traceInfo))
+    logger.info(objectMapper.writeValueAsString(traceInfo))
     inMemoryTraceRepository.add(traceInfo)
   }
 }
